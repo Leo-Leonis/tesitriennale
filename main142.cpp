084bf502aa971a8b7388706fee280432f1e3dc83
Leo-Leonis <yeleandro@icloud.com>
2024-10-08 03:56:48 +0200

    raffinazioni per il codice, generazione grafici e tanto altro (scusate la scarsità di informazioni ma sono le tre di notte e ho sonno)

--- main142.cpp
+++ main142.cpp
@@ -27,34 +27,35 @@
 // prova con 1e6
 int main142(const int nevs = 1000) {
 
+  int n_bins = 200; // il numero dei bin
+
   TH1D *h_pt_proton =
       new TH1D("h_pt_proton",
                "Proton transverse momentum distribution;Transverse "
                "momentum #it{p}_{t} [eV];Occurance [eV^{-1}];",
-               100, 0, 10);
+               n_bins, 0, 10);
   TH1D *h_pt_antiproton =
       new TH1D("h_pt_antiproton",
                "Antiproton transverse momentum distribution;Transverse "
                "momentum #it{p}_{t} [eV];Occurance [eV^{-1}];",
-               100, 0, 10);
+               n_bins, 0, 10);
   TH1D *h_pt_deuteron =
       new TH1D("h_pt_deuteron",
                "Deuteron transverse momentum distribution;Transverse "
                "momentum #it{p}_{t} [eV];Occurance [eV^{-1}];",
-               100, 0, 10);
+               n_bins, 0, 10);
   TH1D *h_pt_antideuteron =
       new TH1D("h_pt_antideuteron",
                "Antideuteron transverse momentum distribution;Transverse "
                "momentum #it{p}_{t} [eV];Occurance [eV^{-1}];",
-               100, 0, 10);
-  // per tutti gli altri istogrammi
+               n_bins, 0, 10);
 
-  // Generator. Process selection. LHC initialization.
+  // Inizializzazione impostazioni di pythia
   Pythia8::Pythia pythia;
   pythia.readString("Beams:eCM = 13000.");
   pythia.readString("Beams:idA =  2212");
   pythia.readString("Beams:idB =  2212");
-  pythia.readString("Beams:eCM = 13600.");
+  // pythia.readString("Beams:eCM = 13600.");
   pythia.readString("Beams:frameType  = 1");
 
   pythia.readString("Random:setSeed = on");
@@ -67,7 +68,7 @@ int main142(const int nevs = 1000) {
   pythia.readString("SoftQCD:inelastic = on");
   pythia.readString("HadronLevel:DeuteronProduction = on"); // provare on e off
 
-  // If Pythia fails to initialize, exit with error.
+  // se Pythia si rompe, uccidi il programma.
   if (!pythia.init())
     return 1;
 
@@ -76,17 +77,25 @@ int main142(const int nevs = 1000) {
   for (int iEvent = 0; iEvent < nevs; ++iEvent) {
     if (!pythia.next())
       continue;
-    for (int i = 0; i < event.size(); ++i) {
+    for (int i = 0; i < event.size(); ++i) { // per ogni particella dell'evento
+
+      // la particella i-esima dell'evento del loop
       auto &p = event[i];
+
       //.isResonance() && p.status() == -62) VH.push_back(p);
 
-      if (std::abs(p.y()) > 0.5) // se la rapidità di p è maggiore di 0.5
-        continue;
-      const float pt =
-          std::sqrt(p.px() * p.px() + p.py() * p.py()); // transverse momentum
-      if (!p.isFinal())
+      if (std::abs(p.y()) > 0.5) // se la rapidità di p è maggiore di 0.5 skippa
+                                 // questa particella
         continue;
 
+      if (!p.isFinal()) // skippa se la particella non è finale (?)
+        continue;
+
+      // transverse momentum
+      const float pt = std::sqrt(p.px() * p.px() + p.py() * p.py());
+
+      // Qui riempiamo i nostri istogrammi per protoni, antiprotoni e deutoni e
+      // antideutoni
       switch (p.id()) {
       case 2212: // p+
         h_pt_proton->Fill(pt);
@@ -101,13 +110,18 @@ int main142(const int nevs = 1000) {
         h_pt_antideuteron->Fill(pt);
         break;
       }
-      // Qui riempiamo i nostri istogrammi per protoni, antiprotoni e deutoni e
-      // antideutoni
-      std::cout << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
-                 "\b\b\b\b\b\b\b\b\b\b\b\b";
+      std::cout
+          << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
+             "\b\b\b\b\b\b\b\b\b\b\b\b";
       std::cout << i;
     }
   }
+
+  // copia degli istogrammi non riscalati
+  TH1D *h_proton = new TH1D(*h_pt_proton);
+  TH1D *h_antiproton = new TH1D(*h_pt_antiproton);
+  TH1D *h_deuteron = new TH1D(*h_pt_deuteron);
+  TH1D *h_antideuteron = new TH1D(*h_pt_antideuteron);
 
   // riscalaggio grafici (per poter confrontare queste distribuzioni e quelle di
   // cern)
@@ -116,13 +130,20 @@ int main142(const int nevs = 1000) {
   h_pt_deuteron->Scale(1. / nevs, "width");
   h_pt_antideuteron->Scale(1. / nevs, "width");
 
-  // salvataggio grafici
+  // salvataggio grafici riscalati
   TFile *resultfile = new TFile("main142.root", "RECREATE");
   h_pt_proton->Write();
   h_pt_antiproton->Write();
   h_pt_deuteron->Write();
   h_pt_antideuteron->Write();
 
-  resultfile->Close();
+  // salvataggio grafici NON riscalati
+  TFile *testfile = new TFile("main142_res.root", "RECREATE");
+  h_proton->Write();
+  h_antiproton->Write();
+  h_deuteron->Write();
+  h_antideuteron->Write();
+
+  testfile->Close();
   return 0;
 }
