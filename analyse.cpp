9453fde06c401427b02d73ff5ab35bb9007c565a
Leo-Leonis <yeleandro@icloud.com>
2024-10-23 12:51:57 +0200

    Aggiunto in analyse.cpp la funzionalità di poter confrontare grafici di cern e quelli del protone e antiprotone

--- analyse.cpp
+++ analyse.cpp
@@ -10,16 +10,20 @@
 
 int analyse() {
 
-  gStyle->SetOptStat(0001);
+  gStyle->SetOptStat(10);
   // gStyle->SetOptFit(1111);
 
+  // prendi i file
   TFile *simu_ON_file = new TFile("main142_ON_1e6.root", "READ");
   TFile *simu_OFF_file = new TFile("main142_OFF_1e6.root", "READ");
   TFile *root_file = new TFile("cern_data/proton_pt.root", "READ");
   // root_file->cd();
 
-  TH1D *h_pt_simu_ON_pp = (TH1D *)simu_ON_file->Get("h_pt_tot_pp");
-  TH1D *h_pt_simu_OFF_pp = (TH1D *)simu_OFF_file->Get("h_pt_tot_pp");
+  // prendi gli istogrammi
+  TH1D *h_pt_simu_ON_tot_pp = (TH1D *)simu_ON_file->Get("h_pt_tot_pp");
+  TH1D *h_pt_simu_ON_proton = (TH1D *)simu_ON_file->Get("h_pt_proton");
+  TH1D *h_pt_simu_ON_antiproton = (TH1D *)simu_ON_file->Get("h_pt_antiproton");
+  TH1D *h_pt_simu_OFF_tot_pp = (TH1D *)simu_OFF_file->Get("h_pt_tot_pp");
   //TH1D *h_pt_cern_pp = (TH1D *)root_file->Get("Table 6/Hist1D_y1");
   TGraphAsymmErrors *h_pt_cern_pp = (TGraphAsymmErrors *)root_file->Get("Table 6/Graph1D_y1");
 
@@ -36,28 +40,42 @@ int analyse() {
   h_pt_cern_pp->SetLineColor(kBlack);
   h_pt_cern_pp->SetMarkerSize(1.2);
   
-  // cosmetica simu on
-  h_pt_simu_ON_pp->SetMarkerStyle(22);
-  h_pt_simu_ON_pp->SetLineColor(kBlue);
-  h_pt_simu_ON_pp->SetMarkerColor(kBlue);
-  h_pt_simu_ON_pp->SetMarkerSize(1.4);
+  // cosmetica simu on pp
+  h_pt_simu_ON_tot_pp->SetMarkerStyle(22);
+  h_pt_simu_ON_tot_pp->SetLineColor(kBlue);
+  h_pt_simu_ON_tot_pp->SetMarkerColor(kBlue);
+  h_pt_simu_ON_tot_pp->SetMarkerSize(1.4);
+
+  // fare cosmetica
+  h_pt_simu_ON_proton->SetMarkerStyle(8);
+  h_pt_simu_ON_proton->SetLineColor(kRed);
+  h_pt_simu_ON_proton->SetMarkerColor(kRed);
+
+  h_pt_simu_ON_antiproton->SetMarkerStyle(4);
+  h_pt_simu_ON_antiproton->SetLineColor(kViolet+7);
+  h_pt_simu_ON_antiproton->SetMarkerColor(kViolet+7);
 
   // cosmetica simu off
-  h_pt_simu_OFF_pp->SetMarkerStyle(21);
-  h_pt_simu_OFF_pp->SetMarkerColor(kViolet);
-  h_pt_simu_OFF_pp->SetLineColor(kViolet);
-  h_pt_simu_OFF_pp->SetMarkerSize(1.2);
+  h_pt_simu_OFF_tot_pp->SetMarkerStyle(21);
+  h_pt_simu_OFF_tot_pp->SetMarkerColor(kViolet);
+  h_pt_simu_OFF_tot_pp->SetLineColor(kViolet);
+  h_pt_simu_OFF_tot_pp->SetMarkerSize(1.2);
 
-  // disegna entrambi i grafici sullo stesso canvas
-  h_pt_simu_OFF_pp->Draw("E, P, SAME");
-  h_pt_simu_ON_pp->Draw("E, P, SAME");
+  // disegna i grafici sullo stesso canvas
+  h_pt_simu_ON_tot_pp->Draw("E, P, SAME");
   h_pt_cern_pp->Draw("E, P, SAME");
+  h_pt_simu_OFF_tot_pp->Draw("E, P, SAME");
+  h_pt_simu_ON_tot_pp->Draw("E, P, SAME");
+  h_pt_simu_ON_proton->Draw("E, P, SAME");
+  h_pt_simu_ON_antiproton->Draw("E, P, SAME");
 
   // aggiungi la legenda
   TLegend *leg = new TLegend(.1, .1, .25, .3);
   leg->SetHeader("Key", "C");
-  leg->AddEntry(h_pt_simu_ON_pp, "PYTHIA ON");
-  leg->AddEntry(h_pt_simu_OFF_pp, "PYTHIA OFF");
+  leg->AddEntry(h_pt_simu_ON_tot_pp, "PYTHIA p+#bar{p}");
+  leg->AddEntry(h_pt_simu_ON_proton, "PYTHIA p");
+  leg->AddEntry(h_pt_simu_ON_antiproton, "PYTHIA #bar{p}");
+  leg->AddEntry(h_pt_simu_OFF_tot_pp, "PYTHIA p+#bar{p} OFF");
   leg->AddEntry(h_pt_cern_pp, "ALICE");
   leg->Draw("Same");
 
