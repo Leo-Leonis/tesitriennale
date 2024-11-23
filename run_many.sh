ae202ae39876c587efafa352e8aa7317551c233e
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2024-11-23 19:20:00 +0100

    Fix

--- run_many.sh
+++ run_many.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+seq 1 10 >runs.txt
 seq 1 1000 >runs.txt
 
-root -l -b -q 'main142.cpp++(0)'
+root -l -b -q 'main142.cpp+(0)'
+sleep 1
 date >donelist.txt
-cat "runs.txt" | xargs -n1 -P 112 -I{} sh -c './run_single.sh "$1"' -- {}
+cat "runs.txt" | xargs -n1 -P ${1-100} -I{} sh -c './run_single.sh "$1"' -- {}
--- run_single.sh
+++ run_single.sh
@@ -2,10 +2,10 @@
 
 RunId=${1-0}
 
-echo $RunId
 
 float_result=$(echo "scale=5; $RunId / 100" | bc)
-float_result=$(echo "scale=5; $RunId / 1" | bc)
+#float_result=$(echo "scale=5; $RunId / 1" | bc)
+echo "Running $RunId: sleeping $float_result"
 sleep $float_result
 
 outDir="output/run_${RunId}/"
@@ -18,9 +18,11 @@ else
     ln -s ../../main142* .
 fi
 
-Cmd="root -l -b -q 'main142.cpp+(1000000, 1, ${RunId}, 0)' > log"
-echo $Cmd
-eval $Cmd
+date > log
+
+Cmd="root -l -b -q 'main142.cpp+(1000000, 1, ${RunId}, 0)'"
+echo $Cmd >> log
+eval $Cmd >> log
 
 cd -
 
