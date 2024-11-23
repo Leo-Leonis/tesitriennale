0af7dcea0ff0dd799c567413d70c24fe47ffbfa8
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2024-11-23 18:42:28 +0100

    Fix

--- run_many.sh
+++ run_many.sh
@@ -4,4 +4,4 @@ seq 1 1000 >runs.txt
 
 root -l -b -q 'main142.cpp++(0)'
 date >donelist.txt
-cat "runs.txt" | xargs -n1 -P 112 -I{} sh -c './run.sh "$1"' -- {}
+cat "runs.txt" | xargs -n1 -P 112 -I{} sh -c './run_single.sh "$1"' -- {}
